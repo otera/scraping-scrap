bef03d0515d0d97ada710134d1482ebbb769bd4e
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2023-04-08 15:11:12 +0900

    feat: それっぽく取得出来た所まで

--- scrap_chateraise.py
+++ scrap_chateraise.py
@@ -1,14 +1,15 @@
 import requests
-import csv
+# import pandas as pd
 from bs4 import BeautifulSoup
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class Product:
-    def __init__(self, name, price, spec):
+    def __init__(self, name, price, comment, spec):
         self.name = name
         self.price = price
+        self.comment = comment
         self.spec = spec
 
     def get_name(self):
@@ -16,6 +17,9 @@ class Product:
 
     def get_price(self):
         return self.price
+
+    def get_comment(self):
+        return self.comment
 
     def get_spec(self):
         return self.spec
@@ -25,6 +29,9 @@ class Product:
 
     def set_price(self, price):
         self.price = price
+
+    def set_comment(self, comment):
+        self.comment = comment
 
     def set_spec(self, spec):
         self.spec = spec
@@ -45,13 +52,21 @@ def get_bs(url):
 
 
 def get_details_icebar(target):
-    details = {}
+    spec = {}
 
     soup = get_bs("https://www.chateraise.co.jp/" + target)
 
-    allergy = soup.find(
-        'dl', {'class': 'goods-detail-description block-goods-attr3'})
-    details["アレルギー"] = allergy.find('dd').text
+    # ここいらないかも
+    # allergy = soup.find(
+    #     'dl', {'class': 'goods-detail-description block-goods-attr3'})
+    # # spec["アレルギー"] = allergy.find('dd').text
+    # if allergy is not None:
+    #     spec["アレルギー"] = allergy.find('dd').text
+    # else:
+    #     spec["アレルギー"] = "不明"
+
+    comment = soup.find(
+        "div", {"class": "block-goods-comment1"}).text.replace("\r\n", "").strip()
 
     goods_item_infos = soup.find_all(
         'div', {'class': 'block-goods-item-info'})
@@ -60,15 +75,23 @@ def get_details_icebar(target):
         for dl in dl_list:
             dt = dl.find('dt').text
             dd = dl.find('dd').text
-            details[dt] = dd
+            spec[dt] = dd
 
-    return details
+    return comment, spec
 
 
 # ページに表示されている商品情報をスクレイピングする
 def get_target_icebar(soup):
     pickup_items = soup.find("ul", {"class": "block-pickup-list-p--items"})
-    for item in pickup_items.find_all("li"):
+
+    # デバッグ用
+    for i, item in enumerate(pickup_items.find_all("li")):
+        # 条件1
+        if i >= 1:
+            break
+
+    # 本番用
+    # for item in pickup_items.find_all("li"):
         name = item.find("a", {
             "class": 'js-enhanced-ecommerce-goods-name'}).text
         price = item.find("div", {
@@ -77,10 +100,12 @@ def get_target_icebar(soup):
             "a", {"class": 'js-enhanced-ecommerce-goods-name'}).get('href')
 
         # 詳細情報を取得
-        # spec = get_details_icebar(detail_url)
-        spec = None
+        print("次の商品情報を取得します", name)
+        comment, spec = get_details_icebar(detail_url)
+        # comment = None
+        # spec = None
 
-        icebars.append(Product(name, price, spec))
+        icebars.append(Product(name, price, comment, spec))
 
 
 icebars = []
@@ -100,3 +125,23 @@ while True:
     next_link.find("a")
 
     target_url = "https://www.chateraise.co.jp/" + next_link.find("a")['href']
+
+for icebar in icebars:
+    with open("シャトレーゼ_アイスバー情報.md", mode="a", encoding='utf-8') as file:
+        file.write('# {0}\n'.format(icebar.get_name()))
+
+        file.write('## 価格\n{0}\n\n'.format(icebar.get_price()))
+
+        file.write('## 栄養成分表示・アレルギー\n')
+        spec = icebar.get_spec()
+        _spec = ""
+        if spec is not None:
+            for k, v in spec.items():
+                _spec += "- {}\n".format(k)
+                _spec += "  - {}\n".format(v)
+            file.write(_spec)
+        else:
+            file.write("※取得できませんでした")
+        file.write('\n')
+
+        file.write('## コメント\n{0}\n\n'.format(icebar.get_comment()))
