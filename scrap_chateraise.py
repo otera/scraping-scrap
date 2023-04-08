a3e32945c9822dddbee65f670801187abc4e48f8
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2023-04-08 13:49:20 +0900

    feat: 次のページまで取得できるように変更

--- scrap_chateraise.py
+++ scrap_chateraise.py
@@ -4,54 +4,6 @@ from bs4 import BeautifulSoup
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# 403回避
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-
-
-def no_product():
-    return "※該当商品はオンラインでの取り扱い終了か取得失敗"
-
-
-def get_details_icebar(target):
-    response = requests.get(
-        "https://www.chateraise.co.jp/" + target, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    goods_item_infos = soup.find_all(
-        'div', {'class': 'block-goods-item-info'})
-    for goods_item_info in goods_item_infos:
-        dl_list = goods_item_info.find_all('dl')
-        for dl in dl_list:
-            dt = dl.find('dt').text
-            dd = dl.find('dd').text
-            print(dt)
-            print(dd)
-
-
-def get_target_icebar():
-    # ターゲットURL
-    target = "https://www.chateraise.co.jp/ec/c/cice-bar/"
-    response = requests.get(target, headers=headers)
-
-    # BeautifulSoupオブジェクトを作成
-    soup = BeautifulSoup(response.text, "html.parser")
-    pickup_items = soup.find("ul", {"class": "block-pickup-list-p--items"})
-
-    # li要素を出力
-    for item in pickup_items.find_all("li"):
-        name = item.find("a", {
-            "class": 'js-enhanced-ecommerce-goods-name'}).text
-        price = item.find("div", {
-                          "class": 'block-pickup-list-p--price price js-enhanced-ecommerce-goods-price'}).text
-        detail_url = item.find(
-            "a", {"class": 'js-enhanced-ecommerce-goods-name'}).get('href')
-
-        print(name, price, detail_url)
-
-
-get_target_icebar()
-
 
 class Product:
     def __init__(self, name, price, spec):
@@ -73,3 +25,78 @@ class Product:
 
     def set_price(self, price):
         self.price = price
+
+    def set_spec(self, spec):
+        self.spec = spec
+
+
+def no_product():
+    return "※該当商品はオンラインでの取り扱い終了か取得失敗"
+
+
+def get_bs(url):
+    # 403回避
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    response = requests.get(url, headers=headers)
+    return BeautifulSoup(response.text, "html.parser")
+
+
+def get_details_icebar(target):
+    details = {}
+
+    soup = get_bs("https://www.chateraise.co.jp/" + target)
+
+    allergy = soup.find(
+        'dl', {'class': 'goods-detail-description block-goods-attr3'})
+    details["アレルギー"] = allergy.find('dd').text
+
+    goods_item_infos = soup.find_all(
+        'div', {'class': 'block-goods-item-info'})
+    for goods_item_info in goods_item_infos:
+        dl_list = goods_item_info.find_all('dl')
+        for dl in dl_list:
+            dt = dl.find('dt').text
+            dd = dl.find('dd').text
+            details[dt] = dd
+
+    return details
+
+
+# ページに表示されている商品情報をスクレイピングする
+def get_target_icebar(soup):
+    pickup_items = soup.find("ul", {"class": "block-pickup-list-p--items"})
+    for item in pickup_items.find_all("li"):
+        name = item.find("a", {
+            "class": 'js-enhanced-ecommerce-goods-name'}).text
+        price = item.find("div", {
+                          "class": 'block-pickup-list-p--price price js-enhanced-ecommerce-goods-price'}).text
+        detail_url = item.find(
+            "a", {"class": 'js-enhanced-ecommerce-goods-name'}).get('href')
+
+        # 詳細情報を取得
+        # spec = get_details_icebar(detail_url)
+        spec = None
+
+        icebars.append(Product(name, price, spec))
+
+
+icebars = []
+target_url = "https://www.chateraise.co.jp/ec/c/cice-bar/"
+while True:
+    soup = get_bs(target_url)
+
+    # 必要な情報をスクレイピング
+    get_target_icebar(soup)
+
+    # 次のページがあるかどうかを確認する
+    next_link = soup.find('li', {"class": 'pager-next'})
+    if next_link is None:
+        break
+
+    # 次のページの URL を生成する
+    next_link.find("a")
+
+    target_url = "https://www.chateraise.co.jp/" + next_link.find("a")['href']
